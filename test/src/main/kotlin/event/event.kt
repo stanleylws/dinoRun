b2f982bec5bc994a2387bc12e8bdca5fd066f64a
stanleylws <stanley.lws10@gmail.com>
2021-07-27 02:17:27 +0800

    Provide game event manager for listening and dispatching events

---
+++ test/src/main/kotlin/event/event.kt
@@ -0,0 +1,49 @@
+package event
+
+import com.badlogic.ashley.core.Entity
+import ktx.collections.GdxSet
+import java.util.*
+
+enum class GameEventType {
+    PLAYER_DAMAGED
+}
+
+interface GameEvent
+
+object GameEventPlayerDamaged: GameEvent {
+    lateinit var player: Entity
+    var damage = 0f
+
+    override fun toString(): String {
+        return "GameEventPlayerDamaged(damage=$damage)"
+    }
+}
+
+interface GameEventListener {
+    fun onEvent(type: GameEventType, data: GameEvent? = null)
+}
+
+class GameEventManager {
+    private val listeners = EnumMap<GameEventType, GdxSet<GameEventListener>>(GameEventType::class.java)
+
+    fun addListener(type: GameEventType, listener: GameEventListener) {
+        var eventListeners = listeners[type]
+        if (eventListeners == null) {
+            eventListeners = GdxSet()
+            listeners[type] = eventListeners
+        }
+        eventListeners.add(listener)
+    }
+
+    fun removeListener(type: GameEventType, listener: GameEventListener) {
+        listeners[type]?.remove(listener)
+    }
+
+    fun removeListener(listener: GameEventListener) {
+        listeners.values.forEach { it.remove(listener) }
+    }
+
+    fun dispatchEvent(type: GameEventType, data: GameEvent? = null) {
+        listeners[type]?.forEach { it.onEvent(type, data) }
+    }
+}
